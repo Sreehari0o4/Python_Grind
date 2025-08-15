d2d62ee7b348279609891181a9c7289c5c0470d5
Sreehari0o4 <sreeharisk0o2@gmail.com>
2025-08-15 14:04:23 +0530

    Add fernet module, generate key and encrypt the pws

--- password_manager/passwrd_manager.py
+++ password_manager/passwrd_manager.py
@@ -1,17 +1,31 @@
-master_passwrd = input("What is the maste password? ")
+from cryptography.fernet import Fernet
+
+'''def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)'''
+
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+key = load_key()
+fer = Fernet(key)
 
 def add():
     uname = input("Username: ")
     pwd = input("Password: ")
     with open("passwords.txt", 'a') as f:
-        f.write(uname + "|" + pwd + "\n")
+        f.write(uname + "|" + (fer.encrypt(pwd.encode())).decode() + "\n")
 
 def view():
     with open("passwords.txt", 'r') as f:
         for lines in f.readlines():
             data = lines.rstrip()
             user, passw = data.split("|")
-            print("User:", user, ", Password:", passw)
+            print("User:", user, ", Password:", fer.decrypt(passw.encode()).decode())
 
 while True:
     mode = input("Would you like to Add a new password or View passwords (add,view), q to Quit: ")
