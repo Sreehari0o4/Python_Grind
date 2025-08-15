0e39d6307160a8e00246ef1306a33ed88eb93b8e
Sreehari0o4 <sreeharisk0o2@gmail.com>
2025-08-15 14:09:53 +0530

    Update generation code

--- password_manager/passwrd_manager.py
+++ password_manager/passwrd_manager.py
@@ -1,9 +1,13 @@
 from cryptography.fernet import Fernet
 
-'''def write_key():
+'''
+def write_key():
     key = Fernet.generate_key()
     with open("key.key", "wb") as key_file:
-        key_file.write(key)'''
+        key_file.write(key)
+        
+write_key()
+'''
 
 def load_key():
     file = open("key.key", "rb")
