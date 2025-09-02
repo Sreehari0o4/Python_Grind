c435cd97319db4909bd53dfa3bc6b41c340dbe2b
SREEHARI S KUMAR <150273737+Sreehari0o4@users.noreply.github.com>
2025-09-02 16:44:48 +0530

    Delete Libraries_L4 directory

--- Libraries_L4/emojize.py
+++
@@ -1,6 +0,0 @@
-# This script takes user input and converts emoji aliases to actual emojis using the 'emoji' library.
-
-import emoji
-
-emo = input("Input: ")
-print(f"{emoji.emojize(emo, language = 'alias')}")
